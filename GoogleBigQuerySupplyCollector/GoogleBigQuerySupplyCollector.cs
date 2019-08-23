30870be3adfee9a241f968de5cd982bbfcd422bd
Mikhail Burilov <burilovmv@gmail.com>
2019-08-23 08:56:56 +0700

    BigQuery supply collector and tests

---
+++ GoogleBigQuerySupplyCollector/GoogleBigQuerySupplyCollector.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Bigquery.v2;
+using Google.Apis.Bigquery.v2.Data;
+using Google.Apis.Services;
+using Google.Cloud.BigQuery.V2;
+using S2.BlackSwan.SupplyCollector;
+using S2.BlackSwan.SupplyCollector.Models;
+
+namespace GoogleBigQuerySupplyCollector
+{
+    public class GoogleBigQuerySupplyCollector: SupplyCollectorBase
+    {
+        public override List<string> DataStoreTypes() {
+            return (new[] { "Google BigQuery" }).ToList();
+        }
+
+        public string BuildConnectionString(string certificateFilePath, string project) {
+            return $"Certificate={certificateFilePath};Project={project}";
+        }
+
+        private BigQueryClient BuildClient(DataContainer container) {
+            string certificateFilePath = null;
+            string projectId = null;
+
+            var parts = container.ConnectionString.Split(";");
+            foreach (var part in parts) {
+                if(String.IsNullOrEmpty(part))
+                    continue;
+
+                var keyvalue = part.Split("=");
+                if (keyvalue.Length == 2) {
+                    if (keyvalue[0].Equals("Certificate", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        certificateFilePath = keyvalue[1];
+                    }
+                    else if (keyvalue[0].Equals("Project", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        projectId = keyvalue[1];
+                    }
+                }
+            }
+            
+            return BigQueryClient.Create(projectId, GoogleCredential.FromFile(certificateFilePath));
+        }
+
+        public override List<string> CollectSample(DataEntity dataEntity, int sampleSize) {
+            var samples = new List<string>();
+
+            using (var client = BuildClient(dataEntity.Container)) {
+                var job = client.CreateQueryJob($"SELECT {dataEntity.Name} FROM {dataEntity.Collection.Name} LIMIT {sampleSize}", null);
+                var results = job.GetQueryResults();
+
+                foreach (var result in results) {
+                    samples.Add(result[0].ToString());
+                }
+            }
+
+            return samples;
+        }
+
+        public override List<DataCollectionMetrics> GetDataCollectionMetrics(DataContainer container) {
+            var metrics = new List<DataCollectionMetrics>();
+
+            using (var client = BuildClient(container))
+            {
+                var datasets = client.ListDatasets(client.ProjectId);
+                foreach (var dataset in datasets)
+                {
+                    var tables = dataset.ListTables();
+
+                    foreach (var table in tables)
+                    {
+                        var tblSchema = client.GetTable(table.Reference);
+
+                        metrics.Add(new DataCollectionMetrics() {
+                            Name = $"{table.Reference.DatasetId}.{table.Reference.TableId}",
+                            RowCount = (long)tblSchema.Resource.NumRows.GetValueOrDefault(),
+                            TotalSpaceKB = (decimal)tblSchema.Resource.NumBytes.GetValueOrDefault() / 1024
+                        });
+                        
+                    }
+                }
+            }
+
+            return metrics;
+        }
+
+        private DataType ConvertDataType(string dbDataType) {
+            if ("STRING".Equals(dbDataType)) {
+                return DataType.String;
+            }
+            else if ("BYTES".Equals(dbDataType))
+            {
+                return DataType.ByteArray;
+            }
+            else if ("INTEGER".Equals(dbDataType))
+            {
+                return DataType.Long;
+            }
+            else if ("INT64".Equals(dbDataType))
+            {
+                return DataType.Long;
+            }
+            else if ("FLOAT".Equals(dbDataType))
+            {
+                return DataType.Float;
+            }
+            else if ("FLOAT64".Equals(dbDataType))
+            {
+                return DataType.Float;
+            }
+            else if ("BOOLEAN".Equals(dbDataType))
+            {
+                return DataType.Boolean;
+            }
+            else if ("BOOL".Equals(dbDataType))
+            {
+                return DataType.Boolean;
+            }
+            else if ("TIMESTAMP".Equals(dbDataType))
+            {
+                return DataType.DateTime;
+            }
+            else if ("DATE".Equals(dbDataType))
+            {
+                return DataType.DateTime;
+            }
+            else if ("TIME".Equals(dbDataType))
+            {
+                return DataType.DateTime;
+            }
+            else if ("DATETIME".Equals(dbDataType))
+            {
+                return DataType.DateTime;
+            }
+
+            return DataType.Unknown;
+        }
+
+        private void AddFields(DataContainer container, DataCollection collection, List<DataEntity> entities,
+            IList<TableFieldSchema> fields, string prefix) {
+
+            foreach (var field in fields)
+            {
+                if ("RECORD".Equals(field.Type) || "STRUCT".Equals(field.Type)) {
+                    AddFields(container, collection, entities, field.Fields, $"{prefix}{field.Name}.");
+                }
+                else {
+                    entities.Add(new DataEntity(
+                        $"{prefix}{field.Name}",
+                        ConvertDataType(field.Type),
+                        field.Type,
+                        container,
+                        collection
+                    ));
+                }
+            }
+        }
+
+        public override (List<DataCollection>, List<DataEntity>) GetSchema(DataContainer container) {
+            var collections = new List<DataCollection>();
+            var entities = new List<DataEntity>();
+
+            using (var client = BuildClient(container)) {
+                var datasets = client.ListDatasets(client.ProjectId);
+                foreach (var dataset in datasets) {
+                    var tables = dataset.ListTables();
+
+                    foreach (var table in tables) {
+                        var collection = new DataCollection(container, $"{table.Reference.DatasetId}.{table.Reference.TableId}");
+
+                        var tblSchema = client.GetTable(table.Reference);
+                        AddFields(container, collection, entities, tblSchema.Schema.Fields, "");
+
+                        collections.Add(collection);
+                    }
+                }
+            }
+
+            return (collections, entities);
+        }
+
+        public override bool TestConnection(DataContainer container) {
+            using (var client = BuildClient(container)) {
+                var projects = client.ListProjects();
+
+                foreach (var project in projects) {
+                    if (project.ProjectId.Equals(client.ProjectId)) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ GoogleBigQuerySupplyCollectorTests/GoogleBigQuerySupplyCollectorTests.cs
@@ -0,0 +1,87 @@
+using System;
+using S2.BlackSwan.SupplyCollector.Models;
+using Xunit;
+
+namespace GoogleBigQuerySupplyCollectorTests
+{
+    public class GoogleBigQuerySupplyCollectorTests
+    {
+        private readonly GoogleBigQuerySupplyCollector.GoogleBigQuerySupplyCollector _instance;
+        public readonly DataContainer _container;
+
+        public GoogleBigQuerySupplyCollectorTests()
+        {
+            _instance = new GoogleBigQuerySupplyCollector.GoogleBigQuerySupplyCollector();
+            _container = new DataContainer()
+            {
+                ConnectionString = _instance.BuildConnectionString("service-credentials.json", "ancient-link-250615") //TODO: read project id from json
+            };
+        }
+
+        [Fact]
+        public void DataStoreTypesTest()
+        {
+            var result = _instance.DataStoreTypes();
+            Assert.Contains("Google BigQuery", result);
+        }
+
+        [Fact]
+        public void TestConnectionTest()
+        {
+            var result = _instance.TestConnection(_container);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void GetTableNamesTest()
+        {
+            var (tables, elements) = _instance.GetSchema(_container);
+            Assert.Equal(3, tables.Count);
+            Assert.Equal(156, elements.Count);
+
+            var tableNames = new string[] { "TESTDATASET.LEADS", "TESTDATASET.EMAILS", "TESTDATASET.CONTACTS_AUDIT" };
+            foreach (var tableName in tableNames)
+            {
+                var table = tables.Find(x => x.Name.Equals(tableName));
+                Assert.NotNull(table);
+            }
+        }
+
+        [Fact]
+        public void GetDataCollectionMetricsTest()
+        {
+            var metrics = new DataCollectionMetrics[] {
+                new DataCollectionMetrics()
+                    {Name = "TESTDATASET.EMAILS", RowCount = 200, TotalSpaceKB = 97.15M},
+                new DataCollectionMetrics()
+                    {Name = "TESTDATASET.LEADS", RowCount = 200, TotalSpaceKB = 78.53M},
+                new DataCollectionMetrics()
+                    {Name = "TESTDATASET.CONTACTS_AUDIT", RowCount = 200, TotalSpaceKB = 110.12M},
+            };
+
+            var result = _instance.GetDataCollectionMetrics(_container);
+            Assert.Equal(metrics.Length, result.Count);
+
+            foreach (var metric in metrics)
+            {
+                var resultMetric = result.Find(x => x.Name.Equals(metric.Name));
+                Assert.NotNull(resultMetric);
+
+                Assert.Equal(metric.RowCount, resultMetric.RowCount);
+                Assert.Equal(metric.TotalSpaceKB, resultMetric.TotalSpaceKB, 2);
+            }
+        }
+
+        [Fact]
+        public void CollectSampleTest()
+        {
+            var entity = new DataEntity("FROM_ADDR", DataType.String, "STRING", _container,
+                new DataCollection(_container, "TESTDATASET.EMAILS"));
+
+            var samples = _instance.CollectSample(entity, 2);
+            Assert.Equal(2, samples.Count);
+            Assert.Contains("will@example.com", samples);
+        }
+
+    }
+}
